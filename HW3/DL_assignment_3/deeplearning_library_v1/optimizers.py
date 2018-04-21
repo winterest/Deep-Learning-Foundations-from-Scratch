78556c74c61732671bf5f61a962bfbc277992178
winterest <lxy.mhwy@gmail.com>
2018-04-20 21:10:41 -0500

    Update

---
+++ HW3/DL_assignment_3/deeplearning_library_v1/optimizers.py
@@ -0,0 +1,98 @@
+import autograd.numpy as np
+from autograd import value_and_grad 
+from autograd.misc.flatten import flatten_func
+
+# gradient descent function - inputs: g (input function), alpha (steplength parameter), max_its (maximum number of iterations), w (initialization)
+def gradient_descent(g,alpha_choice,max_its,w,beta,version):
+    # flatten the input function to more easily deal with costs that have layers of parameters
+    g_flat, unflatten, w = flatten_func(g, w) # note here the output 'w' is also flattened
+
+    # compute the gradient function of our input function - note this is a function too
+    # that - when evaluated - returns both the gradient and function evaluations (remember
+    # as discussed in Chapter 3 we always ge the function evaluation 'for free' when we use
+    # an Automatic Differntiator to evaluate the gradient)
+    gradient = value_and_grad(g_flat)
+
+    # run the gradient descent loop
+    weight_history = []      # container for weight history
+    cost_history = []        # container for corresponding cost function history
+    alpha = 0
+    z = np.zeros(w.shape)    # initializing momentum term
+    
+    for k in range(1,max_its+1):
+        # check if diminishing steplength rule used
+        if alpha_choice == 'diminishing':
+            alpha = 1/float(k)
+        else:
+            alpha = alpha_choice
+
+        # evaluate the gradient, store current (unflattened) weights and cost function value
+        cost_eval,grad_eval = gradient(w)
+        weight_history.append(unflatten(w))
+        cost_history.append(cost_eval)
+        
+        if version == 'normalized':
+            grad_norm = np.linalg.norm(grad_eval)
+            if grad_norm == 0:
+                # pick random direction and normalize to have unit legnth
+                grad_eval = 10**-6*np.sign(2*np.random.rand(len(w)) - 1)
+                grad_norm = np.linalg.norm(grad_eval)
+            grad_eval /= grad_norm
+
+        # take gradient descent step
+        z = z * beta + grad_eval
+        w = w - alpha*z
+
+    # collect final weights
+    weight_history.append(unflatten(w))
+    # compute final cost function value via g itself (since we aren't computing 
+    # the gradient at the final step we don't get the final cost function value 
+    # via the Automatic Differentiatoor) 
+    cost_history.append(g_flat(w))  
+    return weight_history,cost_history
+
+# gradient descent function - inputs: g (input function), alpha (steplength parameter), max_its (maximum number of iterations), w (initialization)
+def normalized_gradient_descent(g,alpha_choice,max_its,w):
+    # flatten the input function to more easily deal with costs that have layers of parameters
+    g_flat, unflatten, w = flatten_func(g, w) # note here the output 'w' is also flattened
+
+    # compute the gradient function of our input function - note this is a function too
+    # that - when evaluated - returns both the gradient and function evaluations (remember
+    # as discussed in Chapter 3 we always ge the function evaluation 'for free' when we use
+    # an Automatic Differntiator to evaluate the gradient)
+    gradient = value_and_grad(g_flat)
+
+    # run the gradient descent loop
+    weight_history = []      # container for weight history
+    cost_history = []        # container for corresponding cost function history
+    alpha = 0
+    for k in range(1,max_its+1):
+        # check if diminishing steplength rule used
+        if alpha_choice == 'diminishing':
+            alpha = 1/float(k)
+        else:
+            alpha = alpha_choice
+
+        # evaluate the gradient, store current (unflattened) weights and cost function value
+        cost_eval,grad_eval = gradient(w)
+        weight_history.append(unflatten(w))
+        cost_history.append(cost_eval)
+
+        # take gradient descent step
+        
+        grad_norm = np.linalg.norm(grad_eval)
+        if grad_norm == 0:
+            # pick random direction and normalize to have unit legnth
+            grad_eval = 10**-6*np.sign(2*np.random.rand(len(w)) - 1)
+            grad_norm = np.linalg.norm(grad_eval)
+        grad_eval /= grad_norm
+            
+        w = w - alpha*grad_eval
+
+    # collect final weights
+    weight_history.append(unflatten(w))
+    # compute final cost function value via g itself (since we aren't computing 
+    # the gradient at the final step we don't get the final cost function value 
+    # via the Automatic Differentiatoor) 
+    cost_history.append(g_flat(w))  
+    return weight_history,cost_history
